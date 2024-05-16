c9b9088e1f9df004c9fb2bf32b8206c70a2055db
Sprevon <sevenuniversity@outlook.com>
2024-05-16 22:15:11 +0800

    feat: 缓存、多搜索和文件上传

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost:3308;dbname=yii_data',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+];
